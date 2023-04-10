b7882fdb54342fffc981a3d215a4274857ebeca2
lukhas27 <lukas.haspel@web.de>
2023-04-10 10:28:44 +0200

    Improved test_data_splitter and added Test Data

--- TestData/test_data_splitter.py
+++ TestData/test_data_splitter.py
@@ -1,20 +1,87 @@
 import os
-import shutil
+import sys
 
-CURRENT_DATA_SET = 0
+from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QIntValidator
+from PyQt5.QtWidgets import QMainWindow, QLabel, QLineEdit, QPushButton, QApplication, QMessageBox
 
-cur_dir = os.curdir
-files = os.listdir(cur_dir)
+CURRENT_DIR = os.curdir
 
-csv_files = list()
 
-for file in files:
-    if file.endswith('.csv'):
-        csv_files.append(file)
+# files in current working directory
 
-for file in csv_files:
-    direction = file.split('_')[2].split('.')[0]
-    target_folder = cur_dir + os.path.sep + direction
-    if not os.path.exists(target_folder):
-        os.makedirs(target_folder)
-    os.rename(file, cur_dir + os.path.sep + target_folder + os.path.sep + str(CURRENT_DATA_SET) + '_' + file)
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Set the window title and dimensions
+        self.setWindowTitle('Test_Data_splitter')
+        self.setGeometry(100, 100, 400, 200)
+
+        # Create a label and input field
+        self.label = QLabel('DATASET:\t', self)
+        self.label.move(50, 50)
+        self.input_field = QLineEdit(self)
+        self.input_field.move(150, 50)
+        # Set the validator to accept only integers
+        validator = QIntValidator(self)
+        self.input_field.setValidator(validator)
+
+        # Create a start button
+        self.start_button = QPushButton('Split', self)
+        self.start_button.move(150, 100)
+        self.start_button.clicked.connect(self.start_clicked)
+
+        # Create a text edit widget
+        self.done_label = QLabel('Splitting done', self)
+        self.done_label.setStyleSheet('color: green;')
+        self.done_label.move(150, 150)
+        self.done_label.setVisible(False)
+
+    def start_clicked(self):
+        if self.validate_input():
+            # Get the input from the input field
+            current_data_set = self.input_field.text()
+
+            files_cwd = os.listdir(CURRENT_DIR)  # files im current working directory
+
+            csv_files = list()
+
+            for file in files_cwd:
+                if file.endswith('.csv'):
+                    csv_files.append(file)
+
+            for file in csv_files:
+                direction = file.split('_')[2].split('.')[0]
+                target_folder = CURRENT_DIR + os.path.sep + direction
+                if not os.path.exists(target_folder):
+                    os.makedirs(target_folder)
+                os.rename(file,
+                          CURRENT_DIR + os.path.sep + target_folder + os.path.sep + str(current_data_set) + '_' + file)
+
+            self.done_label.setVisible(True)
+            # Hide the label after 3 seconds
+            QTimer.singleShot(3000, self.hide_label)
+
+    def validate_input(self):
+        # Get the input value from the line edit
+        input_value = self.input_field.text()
+
+        # Check if the input value is valid
+        try:
+            int(input_value)
+            return True
+        except ValueError:
+            QMessageBox.warning(self, 'Error', 'Type in an integer!')
+            return False
+
+    def hide_label(self):
+        self.done_label.setVisible(False)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec_())
